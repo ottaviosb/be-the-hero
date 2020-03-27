77d725ea145c4c51530c7fad3824e424b4232b8a
Otavio <otaviobitencourt@hotmail.com>
2020-03-27 02:00:30 -0300

    Frond-end do app já integrado ao Back-end

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+import Routes from './routes'
+import './global.css';
 
 function App() {
+//  const [counter, setCounter] = useState(0);
+
+ // function increment() {
+ //   setCounter(counter + 1);
+//}
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello World!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <Routes />
   );
 }
 
